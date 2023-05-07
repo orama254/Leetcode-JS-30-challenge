c173ec129827510d96efe0a3408d9f1f822604f8
orama254 <orama209@gmail.com>
2023-05-07 17:28:10 +0300

    Add solution for Day 03

---
+++ 003 — 2665. Counter II.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let count = init;
+
+    const increment = () => ++count;
+
+    const decrement = () => --count;
+
+    const reset = () => (count = init);
+
+
+    return {
+        increment,
+        decrement,
+        reset
+    }
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
